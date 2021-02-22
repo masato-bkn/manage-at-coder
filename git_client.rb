fed6133d79a207ffed1a300fe49be895ab551177
重田 将人 <masato.0928.j@gmail.com>
2021-02-22 22:13:06 +0900

    githubから特定ファイルの中身を取得できることを確認

--- git_client.rb
+++ git_client.rb
@@ -1,4 +1,14 @@
+require 'base64'
 require 'octokit'
 
 client = Octokit::Client.new(access_token: ENV['GIT_ACCESS_TOKEN'])
-p client.user
+
+contents = client.contents('masato-bkn/AtCoderBeginnerContest', path: "043/c.rb")
+
+content = Base64.decode64(contents.content).force_encoding("UTF-8")
+problem_info = content.split("\n")
+
+p title = problem_info[1].delete("# title: ")
+p level = problem_info[2].delete("# level: ")
+p url = problem_info[3].delete("# url: ")
+p categories = problem_info[4].delete("# category: ").delete('¥"').delete("[]").split(",")
