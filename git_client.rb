aafa0e53768fbea0a1d793130f3b7b7490d40585
重田 将人 <masato.0928.j@gmail.com>
2021-02-26 09:28:26 +0900

    rails new

--- git_client.rb
+++ git_client.rb
@@ -3,12 +3,16 @@ require 'octokit'
 
 client = Octokit::Client.new(access_token: ENV['GIT_ACCESS_TOKEN'])
 
-contents = client.contents('masato-bkn/AtCoderBeginnerContest', path: "043/c.rb")
+contents = client.contents('masato-bkn/AtCoderBeginnerContest')
 
 content = Base64.decode64(contents.content).force_encoding("UTF-8")
 problem_info = content.split("\n")
 
+p "##########"
 p title = problem_info[1].delete("# title: ")
 p level = problem_info[2].delete("# level: ")
 p url = problem_info[3].delete("# url: ")
 p categories = problem_info[4].delete("# category: ").delete('¥"').delete("[]").split(",")
+p "##########"
+
+
