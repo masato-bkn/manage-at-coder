5aa1de8edc76b83ee2945617707762c958dd5ca2
重田 将人 <masato.0928.j@gmail.com>
2021-02-26 10:02:04 +0900

    rubocop, testフォルダ削除

--- git_client.rb
+++ git_client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'base64'
 require 'octokit'
 
@@ -5,14 +7,12 @@ client = Octokit::Client.new(access_token: ENV['GIT_ACCESS_TOKEN'])
 
 contents = client.contents('masato-bkn/AtCoderBeginnerContest')
 
-content = Base64.decode64(contents.content).force_encoding("UTF-8")
+content = Base64.decode64(contents.content).force_encoding('UTF-8')
 problem_info = content.split("\n")
 
-p "##########"
-p title = problem_info[1].delete("# title: ")
-p level = problem_info[2].delete("# level: ")
-p url = problem_info[3].delete("# url: ")
-p categories = problem_info[4].delete("# category: ").delete('¥"').delete("[]").split(",")
-p "##########"
-
-
+p '##########'
+p title = problem_info[1].delete('# title: ')
+p level = problem_info[2].delete('# level: ')
+p url = problem_info[3].delete('# url: ')
+p categories = problem_info[4].delete('# category: ').delete('¥"').delete('[]').split(',')
+p '##########'
