d7296f32830f319343e499f5f8ed505a198a045e
重田 将人 <masato.0928.j@gmail.com>
2021-02-27 13:09:30 +0900

    requests/problem_specについて作成確認のテストを拡充

--- spec/requests/problem_spec.rb
+++ spec/requests/problem_spec.rb
@@ -8,15 +8,27 @@ RSpec.describe ProblemsController, type: :request do
     response
   end
 
-  it 'returns success' do
-    expect(subject).to have_http_status(:success)
-  end
-
   let :params do
     {
       title: 'hoge',
       level: Enums::Level::A,
       date: Time.zone.now
     }
+  end
+
+  it 'returns success' do
+    expect(subject).to have_http_status(:success)
+  end
+
+  it 'problemが作成されること' do
+    expect { subject }.to change { Problem.count }.from(0).to(1)
+  end
+
+  it 'パラメタで指定した値でproblemが作成されること' do
+    subject
+    problem = Problem.first
+    expect(problem.title).to eq(params[:title])
+    expect(problem.level).to eq(params[:level])
+    expect(problem.date).to eq(params[:date].rfc3339)
   end
 end
