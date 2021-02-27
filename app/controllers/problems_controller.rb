a014c33fd6bdd77083e7f6b6d222f9f6432db971
重田 将人 <masato.0928.j@gmail.com>
2021-02-27 09:53:31 +0900

    POST /comics

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class ProblemsController < ApplicationController
-    def create
-      problem = Problem.create(create_params)
-      render json: problem
-    end
+  def create
+    problem = Problem.create(create_params)
+    render json: problem
+  end
 
-    private
+  private
 
-    def create_params
-        params.permit(:title, :level, :date)
-    end
+  def create_params
+    params.permit(:title, :level, :date)
+  end
 end
