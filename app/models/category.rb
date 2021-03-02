f8b980a7e2b61d0e532f4d5214fb3b98798b074d
重田 将人 <masato.0928.j@gmail.com>
2021-03-02 21:34:31 +0900

    categoyをunique制限を付与
    - 登録時にdowncaseする

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  validates :value, presence: true
+  validates :value, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save { value.strip! }
 end
