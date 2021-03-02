0dae2a1c1f0969c873cad3c62c5039edd4279b30
重田 将人 <masato.0928.j@gmail.com>
2021-03-02 21:37:33 +0900

    problemからcategoriesを取得できるようにした

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Problem < ApplicationRecord
+  has_many :problem_categories
+  has_many :categories, through: :problem_categories
+
   validates :level, inclusion: { in: Enums::Level.values }
 
   bind_inum :level, Enums::Level
