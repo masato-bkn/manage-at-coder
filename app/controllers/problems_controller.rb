4282251f63351fb9d22fcd9e999f7afab7adec55
重田 将人 <masato.0928.j@gmail.com>
2021-03-03 21:19:38 +0900

    POST /problemsをリファクタ

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -2,26 +2,32 @@
 
 class ProblemsController < ApplicationController
   def create
-    categories = params[:categories]
-
     problem_id = 0
 
     ActiveRecord::Base.transaction do
-      categories = categories.map do |category|
-        Category.create!(value: category)
-      end
+      categories = create_category
 
       problem_id = Problem.create!(problem_params).id
 
-      categories.map do |category|
-        ProblemCategory.create!(problem_id: problem_id, category_id: category.id)
-      end
+      create_problem_category(problem_id, categories)
     end
 
     render json: Problem.find(problem_id)
   end
 
   private
+
+  def create_category
+    params[:categories].map do |category|
+      Category.find_or_create_by!(value: category)
+    end
+  end
+
+  def create_problem_category(problem_id, categories)
+    categories.map do |category|
+      ProblemCategory.create!(problem_id: problem_id, category_id: category.id)
+    end
+  end
 
   def category_params
     params.permit(:title, :level, :date)
